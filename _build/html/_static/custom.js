8b3578ca5835cc4b38d82b9d464ee186b2e945dc
hussam <hussam.gz98@gmail.com>
2023-06-29 22:26:59 +0200

    hided code cells and added sections

---
+++ _build/html/_static/custom.js
@@ -0,0 +1,5 @@
+document.addEventListener("DOMContentLoaded", function() {
+    var script = document.createElement("script");
+    script.src = "_static/custom.js";
+    document.head.appendChild(script);
+});
